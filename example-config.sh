20134c61a8b2d8611db7d02cfcb9c54989b02559
stekern <stekern@users.noreply.github.com>
2019-05-13 16:10:05 +0200

    Update example config

--- example-config.sh
+++ example-config.sh
@@ -5,9 +5,6 @@
 # Distributed under terms of the MIT license.
 
 
-# NB: Tilde expansion does not currently work, so you need to use full paths. E.g. '/home/<user>/Documents/' instead of '~/Documents/'
-
-
 # The window to select after tmux has finished setting up
 DEFAULT_WINDOW=1
 # The pane to select after tmux has finished setting up
@@ -16,14 +13,14 @@ DEFAULT_PANE=0
 # User-defined templates
 if [ "$SESSION_NAME" = "example1" ]; then
   # Creates one window with three panes
-  WORKSPACE="/home/$(whoami)/"
-  declare -A WINDOWS=( 
+  WORKSPACE="~"
+  declare -A WINDOWS=(
     [1]="WINDOW_NAME_1;$WORKSPACE;;;;"
   )
 elif [ "$SESSION_NAME" = "example2" ]; then
   # Creates one window with three panes, with commands in the first and second pane
-  WORKSPACE="/home/$(whoami)/"
-  declare -A WINDOWS=( 
+  WORKSPACE="~"
+  declare -A WINDOWS=(
     [1]="WINDOW_NAME_1;$WORKSPACE;echo pane 1/3;echo pane 2/3;;"
   )
 elif [ "$SESSION_NAME" = "example3" ]; then
@@ -32,8 +29,8 @@ elif [ "$SESSION_NAME" = "example3" ]; then
   # One with three panes, with commands in the second pane
   # One with two panes, with commands in the first pane
   # One with one pane, with commands in the first pane
-  WORKSPACE="/home/$(whoami)/"
-  declare -A WINDOWS=( 
+  WORKSPACE="~"
+  declare -A WINDOWS=(
     [1]="WINDOW_NAME_1;$WORKSPACE;echo pane 1/3;;echo pane 3/3"
     [2]="WINDOW_NAME_2;$WORKSPACE;;echo pane 2/3;;"
     [3]="WINDOW_NAME_3;$WORKSPACE;echo pane 1/2;;"
@@ -43,7 +40,7 @@ else
   # This is the default theme to use when no user templates matched the given session name
   # Creates one three-paned window named 'dev' and sets WORKSPACE to the folder the script was called from
   WORKSPACE="$PWD"
-  declare -A WINDOWS=( 
+  declare -A WINDOWS=(
     [1]="dev;$WORKSPACE;;;;"
   )
 fi
